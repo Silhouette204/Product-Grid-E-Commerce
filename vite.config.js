7579854d6c81619d2e3624db3aa4634b06d57621
John Lester Burce <johnlesterburce1604@gmail.com>
2026-05-20 19:17:44 +0800

    Add: Created html pages dir for vite config

--- vite.config.js
+++ vite.config.js
@@ -19,15 +19,30 @@ export default defineConfig ({
   build:{
    rollupOptions:{
     input:{
+      //Navigations
         main: resolve(__dirname, "index.html"),
         about: resolve(__dirname, "about.html"),
         article: resolve(__dirname, "article.html"),
         product: resolve(__dirname, "product.html"),
         contact: resolve(__dirname, "contact.html"),
+        order: resolve(__dirname, "order.html"),
+        archive: resolve(__dirname, "archive.html"),
+        
 
         // PRODUCT-ITEMS
         sysUnit: resolve(__dirname, "product-sysUnit.html"),
-        laptop: resolve(__dirname, "product-laptop.html")
+        laptop: resolve(__dirname, "product-laptop.html"),
+        keyboard: resolve(__dirname, "product-keyboard.html"),
+        mouse: resolve(__dirname, "product-mouse.html"),
+        motherboard: resolve(__dirname, "product-motherboard.html"),
+        ram: resolve(__dirname, "product-ram.html"),
+        psu: resolve(__dirname, "product-psu.html"),
+        cpu: resolve(__dirname, "product-cpu.html"),
+        gpu: resolve(__dirname, "product-gpu.html"),
+        cooler: resolve(__dirname, "product-cooler.html"),
+        storage: resolve(__dirname, "product-storage.html"),
+        fan: resolve(__dirname, "product-fan.html"),
+        case: resolve(__dirname, "product-case.html")
     }
    }
   }
