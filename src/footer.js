50a5f83ce0e04c7f22f1b26ef6c17e82204e3f42
John Lester Burce <johnlesterburce1604@gmail.com>
2026-05-28 18:37:11 +0800

    Fix: User Footer Navigation and Links

--- src/footer.js
+++ src/footer.js
@@ -17,52 +17,61 @@ export function renderFooter (){
             <li class="footer-links"> <a href="./index.html" class="text-primary hover:text-dark">Home</a></li>
             <li class="footer-links"><a href="./about.html" class="text-primary hover:text-dark">About</a></li>
             <li class="footer-links"><a href="./product.html" class="text-primary hover:text-dark">Products</a></li>
-            <li class="footer-links"><a href="./services.html" class="text-primary hover:text-dark">Services</a></li>
+            <li class="footer-links"><a href="./articles.html" class="text-primary hover:text-dark">Articles</a></li>
              <li class="footer-links"><a href="./contact.html" class="text-primary hover:text-dark">Contact</a></li>
           </ul>
        </div>
 
-      <div>
-          <h3 class="text-xl text-primary font-semibold mt-5 mb-2 md:text-2xl">Navlinks</h3>
+
+           <div>
+          <h3 class="text-xl text-primary font-semibold mt-5 mb-2 md:text-2xl">Products</h3>
           <ul class="flex flex-col gap-3 my-5">
-            <li class="footer-links"> <a href="#" class="text-primary hover:text-dark">Home</a></li>
-            <li class="footer-links"><a href="#" class="text-primary hover:text-dark">About</a></li>
-            <li class="footer-links"><a href="#" class="text-primary hover:text-dark">Products</a></li>
-            <li class="footer-links"><a href="#" class="text-primary hover:text-dark">Services</a></li>
-             <li class="footer-links"><a href="#" class="text-primary hover:text-dark">Contact</a></li>
+            <li class="footer-links"> <a href="#" class="text-primary hover:text-dark text-base">System Unit</a></li>
+            <li class="footer-links"><a href="#" class="text-primary hover:text-dark text-base">Laptop</a></li>
+            <li class="footer-links"><a href="#" class="text-primary hover:text-dark text-base">Monitor</a></li>
+            <li class="footer-links"><a href="#" class="text-primary hover:text-dark text-base">Keyboard</a></li>
+             <li class="footer-links"><a href="#" class="text-primary hover:text-dark text-base">Mouse</a></li>
+              <li class="footer-links"><a href="#" class="text-primary hover:text-dark text-base">Motherboard</a></li>
+               <li class="footer-links"><a href="#" class="text-primary hover:text-dark text-base">RAM</a></li>
           </ul>
        </div>
 
            <div>
-          <h3 class="text-xl text-primary font-semibold mt-5 mb-2 md:text-2xl">Others</h3>
+          <h3 class="text-xl text-primary font-semibold mt-5 mb-2 md:text-2xl">Products</h3>
           <ul class="flex flex-col gap-3 my-5">
-            <li class="footer-links"> <a href="#" class="text-primary hover:text-dark">Home</a></li>
-            <li class="footer-links"><a href="#" class="text-primary hover:text-dark">About</a></li>
-            <li class="footer-links"><a href="#" class="text-primary hover:text-dark">Products</a></li>
-            <li class="footer-links"><a href="#" class="text-primary hover:text-dark">Services</a></li>
-             <li class="footer-links"><a href="#" class="text-primary hover:text-dark">Contact</a></li>
+            <li class="footer-links"> <a href="#" class="text-primary hover:text-dark">PSU</a></li>
+            <li class="footer-links"><a href="#" class="text-primary hover:text-dark">CPU</a></li>
+              <li class="footer-links"><a href="#" class="text-primary hover:text-dark">GPU</a></li>
+            <li class="footer-links"><a href="#" class="text-primary hover:text-dark">Cooler</a></li>
+            <li class="footer-links"><a href="#" class="text-primary hover:text-dark">Storage</a></li>
+             <li class="footer-links"><a href="#" class="text-primary hover:text-dark">Fan</a></li>
+             <li class="footer-links"><a href="#" class="text-primary hover:text-dark">Case</a></li>
           </ul>
        </div>
 
-           <div>
-          <h3 class="text-xl text-primary font-semibold mt-5 mb-2 md:text-2xl">Navlinks</h3>
+    <div>
+          <h3 class="text-xl text-primary font-semibold mt-5 mb-2 md:text-2xl">Others Projects</h3>
           <ul class="flex flex-col gap-3 my-5">
-            <li class="footer-links"> <a href="#" class="text-primary hover:text-dark">Home</a></li>
-            <li class="footer-links"><a href="#" class="text-primary hover:text-dark">About</a></li>
-            <li class="footer-links"><a href="#" class="text-primary hover:text-dark">Products</a></li>
-            <li class="footer-links"><a href="#" class="text-primary hover:text-dark">Services</a></li>
-             <li class="footer-links"><a href="#" class="text-primary hover:text-dark">Contact</a></li>
+            <li class="footer-links"> <a href="https://responsive-e-commerce-lemon.vercel.app/" class="text-primary hover:text-dark">Watch Commerce</a></li>
+            <li class="footer-links"><a href="https://responsive-ecommerce-landing-page-pi.vercel.app/" class="text-primary hover:text-dark">Project 3</a></li>
+            <li class="footer-links"><a href="https://phase1-data-and-toggle-ui.vercel.app/" class="text-primary hover:text-dark">Project 2</a></li>
+            <li class="footer-links"><a href="https://responsive-landing-page-website.vercel.app/" class="text-primary hover:text-dark">Project 1</a></li>
+            <li class="footer-links"><a href="https://multi-section-lime.vercel.app" class="text-primary hover:text-dark">Practice 2</a></li>
+             <li class="footer-links"><a href="https://responsive-landing-page-psi-orcin.vercel.app/" class="text-primary hover:text-dark">Practice 1</a></li>
           </ul>
        </div>
 
            <div>
           <h3 class="text-xl text-primary font-semibold mt-5 mb-2 md:text-2xl">Socials</h3>
           <ul class="flex flex-row gap-3 my-5">
-            <li class="social-links"> <a href="#" class="text-primary hover:text-dark"><i class="fa-brands fa-github"></i></a></li>
-            <li class="social-links"><a href="#" class="text-primary hover:text-dark"><i class="fa-brands fa-facebook"></i></a></li>
-            <li class="social-links"><a href="#" class="text-primary hover:text-dark"><i class="fa-brands fa-square-instagram"></i></a></li>
-            <li class="social-links"><a href="#" class="text-primary hover:text-dark"><i class="fa-brands fa-square-x-twitter"></i></a></li>
-             <li class="social-links"><a href="#" class="text-primary hover:text-dark"><i class="fa-brands fa-youtube"></i></a></li>
+            <li class="social-links"> <a href="https://github.com/Silhouette204" class="text-primary hover:text-dark"><i class="fa-brands fa-github"></i></a></li>
+            <li class="social-links"><a href="https://www.facebook.com/johnlester.burce/" class="text-primary hover:text-dark"><i class="fa-brands fa-facebook"></i></a></li>
+            <li class="social-links"><a href="https://x.com/JohnWhiff3419" class="text-primary hover:text-dark"><i class="fa-brands fa-square-x-twitter"></i></a></li>
+             <li class="social-links"><a href="https://www.figma.com/files/team/1338002723000708316/all-projects?fuid=1338002720927057932" class="text-primary hover:text-dark"><i class="fa-brands fa-figma"></i></a></li>
+              <li class="social-links"><a href="https://vercel.com/silhouette204s-projects" class="text-primary hover:text-dark text-sm">VERCEL</a></li>
+ 
+               <li class="social-links"><a href="https://app.netlify.com/teams/silhouette204/projects" class="text-primary hover:text-dark text-sm">NETLIFY</a></li>
+        
           </ul>
        </div>
 
